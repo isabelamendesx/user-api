33435eed6129c2ab2729a68355d73e2bd5ad897c
Isabela Mendes <isabela.mendes@me.com.br>
2024-08-14 14:58:03 -0300

    feat - configure dependecy injection extension methods

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Users.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterApplicationUseCases(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        return services;
+    }
+}
---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Users.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterApplicationExternalDependencies(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
