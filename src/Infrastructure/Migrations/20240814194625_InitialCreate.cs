6916c855eeb37c1fa99d3c8e9e67e0b3a2e7c7d6
Isabela Mendes <isabela.mendes@me.com.br>
2024-08-14 16:49:36 -0300

    feat - add first migration

---
+++ src/Infrastructure/Migrations/20240814194625_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "character varying(99)", maxLength: 99, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(99)", maxLength: 99, nullable: false),
+                    nickname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    idd = table.Column<string>(type: "character varying(4)", maxLength: 4, nullable: true),
+                    phone = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    active = table.Column<bool>(type: "boolean", nullable: false),
+                    created_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_created_date",
+                table: "users",
+                column: "created_date");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_updated_date",
+                table: "users",
+                column: "updated_date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
